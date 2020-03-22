03943905370f325b2bd0488b4b5f2a307d99b0e8
Billy Li <billy.li.901@gmail.com>
2020-03-21 23:20:30 -0700

    component parts

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import styled from '@emotion/styled'
+import './homepage.component.scss'
+import CardsContainer from '../../components/cards-container/item-cards-container.component'
+
+
+export const HomePage = () => {
+    return (
+    <div className = 'homepage'>
+        <CardsContainer />
+    </div>
+    )
+}
+
