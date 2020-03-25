57289c4355e629ec79d3145ce665b999d18a706d
Billy Li <billy.li.901@gmail.com>
2020-03-25 11:10:46 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import  {HomePage} from './pages/homepage/homepage';
-import ShopPage from './pages/shop/shop.component'
+import { HomePage } from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop.component';
+import { Header } from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         {' //Switch 只会match第一个 path, 后面再多的都不会match '}
         <Route exact path="/" component={HomePage} />
--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -2,24 +2,21 @@ import React from 'react';
 import './collection-item.component.scss';
 
 export const CollectionItem = ({
-  id, name, price, imageUrl,
-}) => {
-  console.log('imageUrl', imageUrl);
-  return (
-    <div className="collection-item">
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-
+  name, price, imageUrl,
+}) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
-  );
-};
+
+  </div>
+);
 
 export default CollectionItem;
