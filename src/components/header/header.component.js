f08f6353cd749a89fbd2a3452ad6fe7faa3baa32
Billy Li <billy.li.901@gmail.com>
2020-03-27 20:36:08 -0700

    env variable

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -4,31 +4,28 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../asset/logo/logo.svg'; // this syntax is just for when import SVG in React
 import { auth } from '../../firebase/filrebase.util';
 
-export const Header = ({ currentSession }) => {
-  console.log(currentSession);
-  return (
-    <div className="header">
-      <Link className="logo-container" to="/">
-        <Logo className="logo" />
+export const Header = ({ currentSession }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
       </Link>
-      <div className="options">
-        <Link className="option" to="/shop">
-          SHOP
+      <Link className="option" to="/contact">
+        CONTACT
+      </Link>
+      {currentSession ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
         </Link>
-        <Link className="option" to="/contact">
-          CONTACT
-        </Link>
-        {currentSession ? (
-          <div className="option" onClick={() => auth.signOut()}>
-            Sign Out
-          </div>
-        ) : (
-          <Link className="option" to="/signin">
-            SIGN IN
-          </Link>
-        ) }
-      </div>
+      ) }
     </div>
-  );
-};
+  </div>
+);
 export default Header;
