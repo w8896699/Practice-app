c495472bfe7b2da5a7aefde2ca471233535de5ae
Billy Li <billy.li.901@gmail.com>
2020-03-30 17:11:12 -0700

    add redux into header and app.js for user action

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import { HomePage } from './pages/homepage/homepage';
+import { connect } from 'react-redux';
+
+import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop.component';
 import { SignInSignUpPage } from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import { Header } from './components/header/header.component';
+import Header from './components/header/header.component'; // 不能 import {Header}, otherwise mapStateToProps wont get any state from reducer
 import { auth, createUserProfileDocument } from './firebase/filrebase.util';
 
-export const App = () => {
-  const [curUser, setCurUser] = useState(null);
+import * as userAction from './redux/user/user.action';
 
-  useEffect(() => {
-    auth.onAuthStateChanged(async (userAuth) => {
+class App extends React.Component {
+  unsubscribeFromAuth = null;
+
+  // 这里我曾经用的useeffect,但是用了redux之后好像就不能用了,在我用use effect的时候,有个curuser的触发条件很奇怪,明明没有设置却会被触发
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) { // only if user is login in (for now with google)
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => { // get snapshot from firebase and put in our state for all
-          setCurUser({
+          setCurrentUser({
             id: snapShot.id,
             ...snapShot.data(),
           });
         });
       } else {
-        setCurUser(userAuth); // this is for set back our user to null
+        setCurrentUser(userAuth);// this is for set back our user to null
       }
     });
-  }, []);// 这里useEffect我没有把curUser设置为触发条件,具体原因我也没搞懂, 所以按我目前的理解,这个useEffect不会被触发才对,但他就是会被触发....
+  }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
-  return (
-    <div className="App">
-      <Header currentSession={curUser} />
-      <Switch>
-        {' //Switch 只会match第一个 path, 后面再多的都不会match '}
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInSignUpPage} />
-      </Switch>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          {' //Switch 只会match第一个 path, 后面再多的都不会match '}
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(userAction.setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import './header.style.scss';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import { ReactComponent as Logo } from '../../asset/logo/logo.svg'; // this syntax is just for when import SVG in React
 import { auth } from '../../firebase/filrebase.util';
 
-export const Header = ({ currentSession }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
+const Header = ({ currentUser }) => {
+  console.log('currentUser', currentUser);
+  return ( // now reciving current session from reducer
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
       </Link>
-      <Link className="option" to="/contact">
-        CONTACT
-      </Link>
-      {currentSession ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          Sign Out
-        </div>
-      ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
         </Link>
-      ) }
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            Sign Out
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        ) }
+      </div>
     </div>
-  </div>
-);
-export default Header;
+  );
+};
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+export default connect(mapStateToProps)(Header); // connect is a higher component that gets mapStateToProps() an=====-= to get the state value.;
