db686943356ec5a14b2f9429eaf13b3f035334ca
Billy Li <billy.li.901@gmail.com>
2020-04-04 13:41:15 -0700

    presist

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import App from './App';
 
-import store from './redux/store';
+
 // import * as serviceWorker from './serviceWorker';
 import './index.css';
-
+// presist will fire a event to check if windows storage has data
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger'; // useful debug middleware
 
 import rootReducer from './root-reducer';
@@ -7,5 +8,5 @@ import rootReducer from './root-reducer';
 const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));// current we only one midware,
-
-export default store;
+export const persistor = persistStore(store);
+export default { store, persistor };
