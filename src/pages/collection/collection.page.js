5e0f90bbfd06430df94c151aac14bf5a22507a7a
Billy Li <billy.li.901@gmail.com>
2020-04-05 17:16:28 -0700

    idk what i have done...

---
+++ src/pages/collection/collection.page.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import './category.style.scss';
+import { selectCollection } from '../../redux/shop/shop.selector';
+
+const CollectionPage = ({ collection }) => {
+  // const collection = match.param.collectionID;
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+
+      <h2 className="title">
+        {title}
+      </h2>
+      <div className="items">
+        {items.map((item) => (<CollectionItem key={item.id} item={item} />))}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  // selectCollection is a function that returns a function
+  collection: selectCollection(ownProps.match.params.collectionID)(state), // this is necessary because unlike otherselectors, this selector needs a part of the state depending on the url parameter
+});
+
+export default connect(mapStateToProps)(CollectionPage);
