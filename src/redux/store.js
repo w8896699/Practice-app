b9803ea7c988ce4e5248256ba379369d5b4a68c0
Billy Li <billy.li.901@gmail.com>
2020-04-08 20:33:37 -0700

    rebuild

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,8 @@ import logger from 'redux-logger'; // useful debug middleware
 
 import rootReducer from './root-reducer';
 
+
+
 const middlewares = [];
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
