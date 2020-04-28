d38585bcbb778f0b3f7993fab5465ed09b673108
Billy Li <billy.li.901@gmail.com>
2020-04-27 17:50:49 -0700

    new pr, revert

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -6,7 +6,6 @@ import CardsContainer from '../../components/cards-container/item-cards-containe
 
 const HomePage = () => (
   <div className="homepage">
-    <h1>This is a test</h1>
     <CardsContainer />
   </div>
 );
